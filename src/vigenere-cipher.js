432727bd2e26751d771c0901629481f65752073f
KateGomel <ykatuay@gmail.com>
2025-01-04 21:20:48 +0300

    feat: add solution vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,79 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  process(message, key, encrypt = true) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const formattedMessage = message.toUpperCase();
+    const formattedKey = key.toUpperCase();
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < formattedMessage.length; i += 1) {
+      const char = formattedMessage[i];
+      const messageCode = char.charCodeAt(0);
+
+      if (messageCode >= 65 && messageCode <= 90) {
+        const keyCode =
+          formattedKey[keyIndex % formattedKey.length].charCodeAt(0);
+        let newCode;
+
+        if (encrypt) {
+          newCode = ((messageCode - 65 + (keyCode - 65)) % 26) + 65;
+        } else {
+          newCode = ((messageCode - 65 - (keyCode - 65) + 26) % 26) + 65;
+        }
+
+        result += String.fromCharCode(newCode);
+
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    if (this.isDirect) {
+      return result;
+    } else {
+      return result.split("").reverse().join("");
+    }
+  }
+
+  encrypt(message, key) {
+    return this.process(message, key, true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    return this.process(encryptedMessage, key, false);
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };

3c4463b5e25580165f163054ace27c6e7e7c8497
KateGomel <ykatuay@gmail.com>
2025-10-28 12:35:26 +0300

    feat: add solution minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,50 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  let result = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    result.push([]);
+
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      result[i].push(0);
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (matrix[i][j]) {
+        result[i][j] = 1;
+      } else {
+        let count = 0;
+
+        for (let x = -1; x <= 1; x += 1) {
+          // строки
+          for (let y = -1; y <= 1; y += 1) {
+            // столбцы
+
+            if (
+              i + x >= 0 && // верхняя граница не выходит за матрицу
+              i + x < matrix.length && //нижняя граница не выходит за матрицу
+              j + y >= 0 && // не выходим за левый край
+              j + y < matrix[i].length // не выходим за правый край
+            ) {
+              if (matrix[i + x][j + y]) {
+                count += 1;
+              }
+            }
+          }
+        }
+
+        result[i][j] = count;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };

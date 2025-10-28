6ef5f9891e1433ed7fed8b663461b787f2c99a9e
KateGomel <ykatuay@gmail.com>
2025-10-28 13:02:50 +0300

    feat: add solutions transform-array & vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,38 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      if (i < arr.length - 1) {
+        i++;
+      }
+    } else if (arr[i] === "--discard-prev") {
+      if (result.length > 0 && arr[i - 2] !== "--discard-next") {
+        result.pop();
+      }
+    } else if (arr[i] === "--double-next") {
+      if (i < arr.length - 1) {
+        result.push(arr[i + 1]);
+      }
+    } else if (arr[i] === "--double-prev") {
+      if (result.length > 0 && arr[i - 2] !== "--discard-next") {
+        result.push(result[result.length - 1]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,14 +20,56 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  process(message, key, encrypt = true) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const formattedMessage = message.toUpperCase();
+    const formattedKey = key.toUpperCase();
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < formattedMessage.length; i += 1) {
+      const char = formattedMessage[i];
+      const messageCode = char.charCodeAt(0);
+
+      if (messageCode >= 65 && messageCode <= 90) {
+        const keyCode =
+          formattedKey[keyIndex % formattedKey.length].charCodeAt(0);
+        let newCode;
+
+        if (encrypt) {
+          newCode = ((messageCode - 65 + (keyCode - 65)) % 26) + 65;
+        } else {
+          newCode = ((messageCode - 65 - (keyCode - 65) + 26) % 26) + 65;
+        }
+
+        result += String.fromCharCode(newCode);
+
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+
+    if (this.isDirect) {
+      return result;
+    } else {
+      return result.split("").reverse().join("");
+    }
+  }
+
+  encrypt(message, key) {
+    return this.process(message, key, true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    return this.process(encryptedMessage, key, false);
   }
 }
 

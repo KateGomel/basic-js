5fa4ee914662d9254ed826dbf33737f7df196226
KateGomel <ykatuay@gmail.com>
2025-10-28 12:56:48 +0300

    feat: add solutions simple-chain, sort-by-height, sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,22 @@ const { NotImplementedError } = require('../lib');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+  let result = n
+    .toString()
+    .split("")
+    .reduce((acc, item) => acc + +item, 0);
+
+  while (result > 9) {
+    result = result
+      .toString()
+      .split("")
+      .reduce((acc, item) => acc + +item, 0);
+  }
+
+  return result;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
